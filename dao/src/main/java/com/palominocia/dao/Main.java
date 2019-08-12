a77b0818523f76687767e930a9dd4ac4fcd8bb5b
Jonathan <jonathan.palomino.vilca@gmail.com>
2019-08-12 01:18:58 -0500

    Corregido transparencia Fragmento

--- dao/src/main/java/com/palominocia/dao/Main.java
+++ dao/src/main/java/com/palominocia/dao/Main.java
@@ -8,11 +8,22 @@ public class Main {
     public static void main(String args[]) throws Exception {
         Schema schema = new Schema(1, "com.palominocia.dao");
 
-        Entity note= schema.addEntity("Medicos");
-        note.addIdProperty();
-        note.addStringProperty("codMedico").notNull();
-        note.addStringProperty("comment");
-        note.addDateProperty("date");
+        Entity clinicas= schema.addEntity("Clinicas");
+        Entity sedes= schema.addEntity("Sedes");
+        Entity medico= schema.addEntity("Medicos");
+        Entity especialidad= schema.addEntity("Especialidad");
+        Entity citas= schema.addEntity("Citas");
+        Entity diagnostico= schema.addEntity("Diagnostico");
+        Entity receta= schema.addEntity("Receta");
+        Entity medicinas= schema.addEntity("Medicinas");
+
+        clinicas.addIntProperty("codigoClinica").primaryKey().autoincrement();
+        clinicas.addStringProperty("NombreClinica");
+
+        sedes.addIntProperty("codigoSede").primaryKey().autoincrement();
+        sedes.addStringProperty("NombreSede");
+
+        clinicas.addToMany(sedes,sedes.getPkProperty());
 
         new DaoGenerator().generateAll(schema, "./dao/src/main/java");
     }
